64afb6b5b0f5ff5c2b960de28a4a979a43ddf958
Derek Harding <derek.robert.harding@gmail.com>
2025-02-27 17:17:32 -0500

    RequestedThemeChanged and logging

--- maui-theme-change-issue/App.xaml.cs
+++ maui-theme-change-issue/App.xaml.cs
@@ -1,10 +1,32 @@
-﻿namespace maui_theme_change_issue
+﻿using System.Diagnostics;
+
+namespace maui_theme_change_issue
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+
+            Application.Current.RequestedThemeChanged += async (sender, appThemeChangedEventArgs) =>
+            {
+                Debug.WriteLine($"Theme change: {appThemeChangedEventArgs.RequestedTheme}");
+            };
+        }
+
+        protected override void OnStart()
+        {
+            Debug.WriteLine("App.OnStart");
+        }
+
+        protected override void OnResume()
+        {
+            Debug.WriteLine("App.OnResume");
+        }
+
+        protected override void OnSleep()
+        {
+            Debug.WriteLine("App.OnSleep");
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
